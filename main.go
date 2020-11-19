07de602c3e7f7b35bf3290f6131235eec576aa54
Hugo <61120457+BlazingRuns@users.noreply.github.com>
2020-11-19 16:14:00 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+        "github.com/BlazingRuns/go-pgen"
+)//go get github.com/BlazingRuns/go-pgen
+
+var (
+	lowerCharSet   = "abcdedfghijklmnopqrst"
+	upperCharSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialCharSet = "!@#$%&*"
+	numberSet      = "0123456789"
+	allCharSet     = lowerCharSet + upperCharSet + specialCharSet + numberSet
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+	minSpecialChar := 1
+	minNum := 1
+	minUpperCase := 1
+	passwordLength := 12
+	password := generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase)
+	fmt.Println(password)
+}
